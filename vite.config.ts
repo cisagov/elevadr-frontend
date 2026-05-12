1533c80bdf0a6225d3d73d9fc32e3a1718b6fda0
Bailey Nelson <bdnels@sandia.gov>
2026-05-12 19:52:15 +0000

    chore(fonts) added the fonts from uswds; build(vite) fixed vite config behaviors and path expectations; build(deps) removed husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
 
   // ---------- Vite options ----------
-
 
   // ---------- Vitest options ----------
   test: {
